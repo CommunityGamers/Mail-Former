a8b778faae502c6bf92048f7da90907e5fd04cfb
Soxox <36239038+CommunityGamers@users.noreply.github.com>
2018-02-19 20:45:34 +0100

    Add files via upload

---
+++ Entbannungsantrag.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NTF2DNN');
+
+    </script>
+    <!-- End Google Tag Manager -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111742062-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-111742062-1');
+
+    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <meta name="copyrighted-site-verification" content="bb731baeda3f7f96" />
+    <link rel="stylesheet" href="css/footer.css">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/Kontakt.css">
+    <title>CG|Entbannungsantrag</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <style type="text/css">
+        body {
+            padding-top: 5rem;
+        }
+
+        .starter-template {
+            padding: 3rem 1.5rem;
+            text-align: center;
+        }
+
+    </style>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTF2DNN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="index.html">CG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Discord.html">Discord</a>
+                </li>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Seiten</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="support.html">Support</a>
+                        <a class="dropdown-item" href="Regeln.html">Regeln</a>
+                        <a class="dropdown-item" href="Informationen.html">Informationen</a>
+                        <a class="dropdown-item" href="Bildergalerie.html">Gallery</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            </form>
+        </div>
+        <a class="btn btn-outline-primary" href="Login.php">Sign up</a>
+    </nav>
+
+    <div class="container">
+        <div class="starter-template">
+            <h1>ComunityGamers</h1>
+            <p class="lead">CommunityGamers Netzwerk</p><br />
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+
+            </div>
+            <div class="container marketing"><br />
+
+                <div id="Entbannungsantrag">
+                    <h1><strong><a>Entbannungsantrag</a><br>
+					  </strong></h1>
+                    <p><strong>Bitte nutze den Entbannungsantrag mit bedacht ausnutzung wird bestraft</strong></p>
+                    <hr>
+
+
+
+                    <form method="post" action="skript.php">
+                        <label>Minecraft-Name*</label><br />
+                        <input type="text" name="Name" required placeholder="Minecraft-Name" class="feedback-input" /><br /><br />
+
+                        <label>Deine Email-Adresse*</label><br />
+                        <input type="email" name="Email" required placeholder="E-Mail" class="feedback-input" /><br /><br />
+
+                        <label>Bangrund*</label><br />
+                        <input type="text" name="Reason" required placeholder="Bangrund" class="feedback-input" /><br /><br />
+
+                        <label>Message*</label><br />
+                        <textarea name="Message" placeholder="Deine Nachricht an uns" cols="60" rows="8" required class="feedback-input"></textarea><br /><br />
+                        <div id="ud_g" class="g-recaptcha" data-sitekey="6LeV20AUAAAAACpTsZ63Kh_WgpfgE9zaFWIqHOyl"></div><br />
+                        <input type="submit" name="submit" value="Send" id="ContactButton" />
+                    </form>
+
+
+                    <!-- Bootstrap core JavaScript ================================================== -->
+                    <!-- Placed at the end of the document so the pages load faster -->
+                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+
+
+                    </script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
