a294d2569f47314c210a1260f6f670d351ebb31f
Soxox <36239038+CommunityGamers@users.noreply.github.com>
2018-02-19 20:43:51 +0100

    Add files via upload

---
+++ gesendet.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NTF2DNN');
+
+    </script>
+    <!-- End Google Tag Manager -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111742062-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-111742062-1');
+
+    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <meta name="copyrighted-site-verification" content="bb731baeda3f7f96" />
+    <link rel="stylesheet" href="css/footer.css">
+
+    <title>CG|Gesendet</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <style type="text/css">
+        body {
+            padding-top: 5rem;
+        }
+
+        .starter-template {
+            padding: 3rem 1.5rem;
+            text-align: center;
+        }
+
+    </style>
+
+</head>
+
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTF2DNN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="index.html">CG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Discord.html">Discord</a>
+                </li>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Seiten</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="support.html">Support</a>
+                        <a class="dropdown-item" href="Regeln.html">Regeln</a>
+                        <a class="dropdown-item" href="Informationen.html">Informationen</a>
+                        <a class="dropdown-item" href="Bildergalerie.html">Gallary</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            </form>
+        </div>
+        <a class="btn btn-outline-primary" href="Login.php">Sign up</a>
+    </nav>
+
+    <div class="container">
+        <div class="starter-template">
+            <h1>ComunityGamers</h1>
+            <p class="lead">CommunityGamers Netzwerk</p><br />
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+
+            </div>
+            <div class="container marketing"><br />
+<br /><br />
+
+                <div class="alert alert-success">
+                    <strong>Geschaft!</strong> Dein Entbannungsantrag wurde erfolgreich erstellt
+                </div><br />
+				<img src="svg/Clock.svg"><br /><br />
+
+
+                <!-- Marketing messaging and featurettes
+      ================================================== -->
+                <!-- Wrap the rest of the page in another container to center all the content. -->
+
+
+                <!-- Three columns of text below the carousel -->
+                <div class="row">
+                    <div class="col-lg-4">
+                        <img src="img/soxox1.png" alt="Generic placeholder image"><br /><br />
+                        <h2><strong>Soxox</strong></h2>
+                        <p><strong>Admin</strong></p>
+                    </div>
+                    <!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img src="img/felary.png" alt="Generic placeholder image"><br /><br />
+                        <h2><strong>Felary</strong></h2>
+                        <p><strong>Admin</strong></p>
+                    </div>
+                    <!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <img src="img/lumin.png" alt="Generic placeholder image">
+                        <h2><strong>xX_Lumin4x_Xx</strong></h2>
+                        <p><strong>Builder</strong></p>
+                    </div>
+                    <!-- /.col-lg-4 -->
+                </div>
+                <!-- /.row -->
+
+
+            </div>
+            <!-- /.container --><br /><br /><br /><br />
+
+            <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+            <div class="footer-bottom">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+                            <div class="copyright">
+                                <a class="copyrighted-badge" title="Copyrighted.com Protection Status" target="_blank" href="https://www.copyrighted.com/website/q9vetkS3oBJqAGOj"><img alt="Copyrighted.com Registered &amp; Protected" border="0" width="150" height="40" src="https://static.copyrighted.com/badges/001.png" /></a>
+                                <script src="https://static.copyrighted.com/badges/helper.js" type="text/javascript"></script>
+                            </div>
+
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+                            <div class="design">
+
+                                <a href="https://www.youtube.com/channel/UCJz6mLLcUbjRUEr3ogbBxoQ?view_as=subscriber">Youtube </a> | <a target="_blank" href="Datenschutz.php">Web Design & Development by CommunityGamers</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- Bootstrap core JavaScript ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
